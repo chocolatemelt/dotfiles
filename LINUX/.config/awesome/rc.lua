1d79f2b8d7b135b646b6add7f26e4b157892e53f
Kevin Zhang <kzhaaang@gmail.com>
2017-04-03 00:06:21 -0400

    remove weird layout entirely...

--- LINUX/.config/awesome/rc.lua
+++ LINUX/.config/awesome/rc.lua
@@ -77,7 +77,8 @@ awful.layout.layouts = {
     -- awful.layout.suit.tile.top,
     -- awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral,
-    lain.layout.centerwork,
+    awful.layout.suit.tile.left,
+    -- lain.layout.centerwork,
     -- awful.layout.suit.spiral.dwindle,
     -- awful.layout.suit.max
 }
